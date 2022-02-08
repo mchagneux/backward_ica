8c6324f1c2d0d5c8463845c074647e9db9fca756
Mathis Chagneux <mathis.chagneux@telecom-paris.fr>
2022-02-08 18:11:46 +0100

    switching to pytorch

--- utils/linear_gaussian_hmm.py
+++ utils/linear_gaussian_hmm.py
@@ -1,36 +1,28 @@
 from utils.misc import * 
-from jax.random import multivariate_normal
-import jax.numpy as jnp
-from jax import jit, random
-from jax.numpy import dot
+import torch
+from torch.distributions.multivariate_normal import MultivariateNormal
 
 
-
-def sample_state_sequence(key, sequence_length, prior_params:Prior, transition_params:Transition):
+def sample_state_sequence(sequence_length, prior_params:Prior, transition_params:Transition):
     dim_z = transition_params.matrix.shape[0]
-    state_sequence = jnp.empty(shape=(sequence_length, dim_z))
-    key, subkey = random.split(key)
-    state_sequence = state_sequence.at[0].set(multivariate_normal(key=subkey, mean=prior_params.mean, cov=prior_params.cov))
+    state_sequence = torch.empty(shape=(sequence_length, dim_z))
+    state_sequence[0] = MultivariateNormal(loc=prior_params.mean, covariance_matrix=prior_params.cov).sample()
     
 
     for sample_nb in range(1, sequence_length):
-        key, subkey = random.split(key)
-        state_sequence = state_sequence.at[sample_nb].set(multivariate_normal(key=subkey, 
-                                                        mean=dot(transition_params.matrix,  state_sequence[sample_nb-1]) + transition_params.offset, 
-                                                        cov=transition_params.cov))
+        state_sequence[sample_nb] = MultivariateNormal(loc=transition_params.matrix @ state_sequence[sample_nb-1] + transition_params.offset, 
+                                                        covariance_matrix=transition_params.cov).sample()
     
-    return key, state_sequence
+    return state_sequence
 
-def sample_joint_sequence(key, sequence_length, model_params:Model):
+def sample_joint_sequence(sequence_length, model_params:Model):
     observation_params:Emission = model_params.emission
     dim_x = observation_params.matrix.shape[0]
-    observation_sequence = jnp.empty(shape=(sequence_length,dim_x))
-    key, state_sequence = sample_state_sequence(key, sequence_length, model_params.prior, model_params.transition)
+    observation_sequence = torch.empty(size=(sequence_length,dim_x))
+    key, state_sequence = sample_state_sequence(sequence_length, model_params.prior, model_params.transition)
     for sample_nb in range(sequence_length):
-        key, subkey = random.split(key)
-        observation_sequence = observation_sequence.at[sample_nb].set(multivariate_normal(key=subkey, 
-                                                                mean=dot(observation_params.matrix, state_sequence[sample_nb]) + observation_params.offset, 
-                                                                cov=observation_params.cov))
+        observation_sequence[sample_nb] = MultivariateNormal(loc=observation_params.matrix @ state_sequence[sample_nb] + observation_params.offset, 
+                                                            covariance_matrix=observation_params.cov).sample()
 
     return state_sequence, observation_sequence
 
