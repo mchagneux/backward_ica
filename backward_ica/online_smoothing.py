found a mistake in the first term of the online elbo, reduces bias but doesn't solve the problem for the gradients

backward_ica/online_smoothing.py
@@ -130,7 +130,7 @@ def init_IS(carry_m1, input_0, p:HMM, q:BackwardSmoother, h_0, num_samples):
     data = {'tm1': carry_m1, 't':data_0}
 
     tau_0 = named_vmap(partial(h_0, models={'p':p, 'q':q}), 
-                    axes_names={'t':{'x':0}}, 
+                    axes_names={'t':{'x':0,'log_q_x':0}}, 
                     input_dict=data)
 
     carry = {'x':x_0,
@@ -481,9 +481,9 @@ def init_gradients_F(carry_m1, input_0, p:HMM, q:BackwardSmoother, h_0, num_samp
     q_0_params = q.filt_params_from_state(s_0, phi_0)
 
     x_0 = jax.vmap(q.filt_dist.sample, in_axes=(0,None))(jax.random.split(key_0, num_samples), q_0_params)
-    unravel = ravel(tree_get_idx(0, carry_m1['stats']['F']))[1]
+    unravel = ravel(unformatted_phi_0)[1]
 
-    def log_q_0(unformatted_phi, x_0):
+    def _log_q_0(unformatted_phi, x_0):
         phi_0 = q.format_params(unformatted_phi)
         s_0 = q.init_state(y_0, phi_0)
         q_0_params = q.filt_params_from_state(s_0, phi_0)
@@ -492,11 +492,19 @@ def init_gradients_F(carry_m1, input_0, p:HMM, q:BackwardSmoother, h_0, num_samp
 
     h = partial(h_0, models={'p':p, 'q':q})
 
-    data = {'tm1': carry_m1,'t':{'x':x_0, 'y':y_0}}
-    H_0 = named_vmap(h, axes_names={'t':{'x':0}}, input_dict=data)
+    def _g_0(unformatted_phi, x_0):
+        log_q_0, grad_log_q_0 = jax.value_and_grad(_log_q_0, argnums=0)(unformatted_phi, x_0)
+        return log_q_0, ravel(grad_log_q_0)[0]
 
-    G_0 = jax.vmap(lambda x,y: ravel(jax.grad(log_q_0, argnums=0)(x,y))[0], in_axes=(None, 0))(unformatted_phi_0, x_0)
-    F_0 = jax.vmap(lambda x,y: x*y)(G_0, H_0)
+    log_q_0, g_0 = jax.vmap(_g_0, in_axes=(None,0))(unformatted_phi_0, x_0)
+
+    data = {'tm1': carry_m1,'t':{'x':x_0, 'y':y_0, 'log_q_x':log_q_0}}
+
+    H_0 = named_vmap(h, axes_names={'t':{'x':0, 'log_q_x':0}}, input_dict=data)
+
+    G_0 = g_0
+
+    F_0 = G_0 * H_0.reshape(-1,1)
 
     carry = {'stats':{'F':jax.vmap(unravel)(F_0), 'G':jax.vmap(unravel)(G_0), 'H':H_0},
             's':s_0, 
@@ -524,7 +532,7 @@ def update_gradients_F(
     G_tm1 = stats_tm1['G']
     F_tm1 = stats_tm1['F']
 
-    unravel = ravel(tree_get_idx(0, F_tm1))[1]
+    unravel = ravel(unformatted_phi_t)[1]
     
     G_tm1 = jax.vmap(lambda x: ravel(x)[0])(G_tm1)
     F_tm1 = jax.vmap(lambda x: ravel(x)[0])(F_tm1)
@@ -572,11 +580,11 @@ def update_gradients_F(
 
         H_t = w_t @ (H_tm1 + h_t)
 
-        G_t = jnp.sum(jax.vmap(lambda w,G,g: w*(G+g))(w_t, G_tm1, g_t), axis=0)
+        G_t = w_t.reshape(-1,1).T @ (G_tm1 + g_t)
 
-        F_t = jnp.sum(jax.vmap(lambda w,F,G,H,g,h: w*(F + g*H + h*G + g*h))(w_t, F_tm1, G_tm1, H_tm1, g_t, h_t), axis=0)
+        F_t = w_t.reshape(-1,1).T @ (F_tm1 + g_t * H_tm1.reshape(-1,1) + h_t.reshape(-1,1) * G_tm1 + g_t*h_t.reshape(-1,1))
 
-        return unravel(F_t), unravel(G_t), H_t
+        return unravel(F_t.squeeze()), unravel(G_t.squeeze()), H_t
 
     F_t, G_t, H_t = jax.vmap(update)(x_t)
 
@@ -585,22 +593,6 @@ def update_gradients_F(
             'x':x_t}
 
     return carry_t
-
-
-def init_carry_gradients_F(unformatted_params, state_dim, obs_dim, num_samples, out_shape, dummy_state):
-
-    dummy_x = jnp.empty((num_samples, state_dim))
-    dummy_H = jnp.empty((num_samples, *out_shape))
-    dummy_F = jax.jacrev(lambda phi:dummy_H)(unformatted_params)
-    dummy_G = jax.jacrev(lambda phi:dummy_H)(unformatted_params)
-
-    carry = {'s': dummy_state, 
-            'x':dummy_x, 
-            'stats':{'H':dummy_H, 
-                    'F':dummy_F, 
-                    'G':dummy_G}}
-
-    return carry
 
 
 

backward_ica/training.py
@@ -65,9 +65,7 @@ class SVITrainer:
                 num_samples=1, 
                 force_full_mc=False,
                 frozen_params=None,
-                online=False,
-                sweep_sequences=False,
-                pairs=False):
+                online=False):
 
         self.num_epochs = num_epochs
         self.batch_size = batch_size
@@ -92,9 +90,7 @@ class SVITrainer:
         zero_grads_optimizer = optax.masked(optax.set_to_zero(), self.fixed_params)
 
         self.optimizer = optax.chain(zero_grads_optimizer, base_optimizer)
-        self.sweep_sequences = sweep_sequences
         self.online = online
-        self.pairs = pairs
 
         if isinstance(self.q, TwoFilterSmoother):
             self.elbo = GeneralForwardELBO(self.p, self.q, num_samples)
@@ -124,74 +120,41 @@ class SVITrainer:
                                                                         compute_up_to, 
                                                                         self.formatted_theta_star, 
                                                                         q.format_params(params))
-    
-        if self.online: 
+        
+            if self.online: 
 
-            self.elbo = OnlineELBOAndGrads(p=self.p, 
-                                    q=self.q, 
-                                    num_samples=num_samples)
-            self.get_montecarlo_keys = get_keys
+                self.elbo = OnlineELBOAndGrads(p=self.p, 
+                                        q=self.q, 
+                                        num_samples=num_samples)
+                self.get_montecarlo_keys = get_keys
 
-            def online_update(carry, key, data, timestep, params):
-                jac_Omega_tm1 = carry['stats']['jac_Omega']
-
-
-                carry['theta'] = self.formatted_theta_star
-
-                input = {'key': key, 
-                        't':timestep, 
-                        'phi':params,
-                        'y':data}
-
-                new_carry = self.elbo.compute(carry, input)[0]
-
-                Omega_t, jac_Omega_t = new_carry['stats']['Omega'], new_carry['stats']['jac_Omega']
-
-                neg_elbo_t = tree_map(lambda x: -jnp.mean(x, axis=0) / (timestep + 1), Omega_t)
-
-                grad_neg_elbo_t = tree_map(lambda x,y: jnp.mean(x-y, axis=0) / (timestep+1), jac_Omega_tm1, jac_Omega_t)
-                
-                return new_carry, (neg_elbo_t, grad_neg_elbo_t)
+                def online_update(carry, key, data, timestep, params):
+                    jac_Omega_tm1 = carry['stats']['jac_Omega']
 
 
+                    carry['theta'] = self.formatted_theta_star
 
-            self.online_update = online_update
-            self.online_init = partial(self.elbo.init_carry, params=self.q.get_random_params(jax.random.PRNGKey(0)))
+                    input = {'key': key, 
+                            't':timestep, 
+                            'phi':params,
+                            'y':data}
 
-        if self.sweep_sequences and (not self.online): 
-            def step(carry, x):
-                def batch_step(params, opt_state, batch, keys):
-                    def timestep_step(carry, x):
-                        params, opt_state, batch, keys = carry 
-                        timestep = x
-                        neg_elbo_values, grads = vmap(value_and_grad(self.loss, argnums=3), 
-                                                        in_axes=(0,0,None,None))(
-                                                                            keys, 
-                                                                            batch, 
-                                                                            timestep, 
-                                                                            params)
+                    new_carry = self.elbo.compute(carry, input)[0]
 
-                        avg_grads = jax.tree_util.tree_map(partial(jnp.mean, axis=0), grads)
-                        updates, opt_state = self.optimizer.update(avg_grads, opt_state, params)
-                        params = optax.apply_updates(params, updates)
-                        return (params, opt_state, batch, keys), -jnp.mean(neg_elbo_values)
+                    Omega_t, jac_Omega_t = new_carry['stats']['Omega'], new_carry['stats']['jac_Omega']
 
-                    (params, opt_state, _, _), neg_elbo_values = jax.lax.scan(timestep_step, 
-                                                                                init=(params, opt_state, batch, keys), 
-                                                                                xs=self.timesteps(batch.shape[1]))
+                    neg_elbo_t = tree_map(lambda x: -jnp.mean(x, axis=0) / (timestep + 1), Omega_t)
+
+                    grad_neg_elbo_t = tree_map(lambda x,y: jnp.mean(x-y, axis=0) / (timestep+1), jac_Omega_tm1, jac_Omega_t)
                     
-                    return params, opt_state, jnp.mean(neg_elbo_values)
+                    return new_carry, (neg_elbo_t, grad_neg_elbo_t)
 
-                data, params, opt_state, subkeys_epoch = carry
-                batch_start = x
-                batch_obs_seq = jax.lax.dynamic_slice_in_dim(data, batch_start, self.batch_size)
-                batch_keys = jax.lax.dynamic_slice_in_dim(subkeys_epoch, batch_start, self.batch_size)
 
-                params, opt_state, avg_elbo_batch = batch_step(params, opt_state, batch_obs_seq, batch_keys)
 
-                return (data, params, opt_state, subkeys_epoch), avg_elbo_batch
+                self.online_update = online_update
+                self.online_init = partial(self.elbo.init_carry, params=self.q.get_random_params(jax.random.PRNGKey(0)))
 
-        elif self.online:
+        if self.online:
 
             init_elbo_carry = vmap(self.online_init, 
                                     axis_size=self.batch_size)()

online_gradient_diagnosis.py
@@ -6,6 +6,7 @@ from functools import partial
 from backward_ica.utils import enable_x64
 import os 
 from jax.tree_util import tree_map
+from jax.flatten_util import ravel_pytree as ravel
 d_x = 2
 d_y = 2
 import pandas as pd 
@@ -36,14 +37,22 @@ q = LinearGaussianHMM(state_dim=d_x,
 
 num_samples = 100
 num_replicas = 50
-seq_length = 50
+seq_length = 2
 
 
 
 key = jax.random.PRNGKey(5)
 
 
-path = 'experiments/online/grad_computation_fixed_V_tm1'
+path = 'experiments/online/online_additive'
+
+os.makedirs(path, exist_ok=True)
+
+def scalar_relative_error(oracle, estimate):
+    return (oracle - estimate) / jnp.abs(oracle)
+
+def cosine_similarity(oracle, estimate):
+    return (oracle @ estimate) / (jnp.linalg.norm(oracle, ord=2) * jnp.linalg.norm(estimate, ord=2))
 
 def experiment(key, exp_id, exp_name):
 
@@ -65,7 +74,7 @@ def experiment(key, exp_id, exp_name):
                                                     p.format_params(theta), 
                                                     q.format_params(phi)), 
                                                     argnums=1)(obs_seq, phi)
-        return elbo, jax.flatten_util.ravel_pytree(grad_elbo)[0]
+        return elbo, ravel(grad_elbo)[0]
 
 
 
@@ -76,7 +85,7 @@ def experiment(key, exp_id, exp_name):
                                                     q.format_params(phi)), 
                                                     argnums=1)(obs_seq, phi)
 
-        return elbo, jax.flatten_util.ravel_pytree(grad_elbo)[0]
+        return elbo, ravel(grad_elbo)[0]
 
 
 
@@ -87,7 +96,7 @@ def experiment(key, exp_id, exp_name):
                                                     phi)['tau'], 
                                                     argnums=1)(obs_seq, phi)
 
-        return elbo, jax.flatten_util.ravel_pytree(grad_elbo)[0]
+        return elbo, ravel(grad_elbo)[0]
 
 
     def online_elbo_and_grad_func_2(key, obs_seq):
@@ -96,7 +105,7 @@ def experiment(key, exp_id, exp_name):
                                                     phi)
         elbo, grad_elbo = stats['H'], stats['F']
 
-        return elbo, jax.flatten_util.ravel_pytree(grad_elbo)[0]
+        return elbo, ravel(grad_elbo)[0]
 
 
 
@@ -107,15 +116,11 @@ def experiment(key, exp_id, exp_name):
     state_seq, obs_seq = p.sample_seq(key_seq, theta, seq_length)
 
     oracle_elbo, oracle_grad_elbo = jax.jit(oracle_elbo_and_grad_func)(obs_seq)
-    norm_oracle_grad = jnp.linalg.norm(oracle_grad_elbo, ord=1)
     # print('Oracle ELBO', oracle_elbo)
     # print('Oracle grad norm ELBO', oracle_grad_elbo)
 
 
     # print('-----')
-    offline_elbos, offline_grads_elbo = jax.jit(jax.vmap(offline_elbo_and_grad_func, in_axes=(0,None)))(jax.random.split(key, num_replicas), obs_seq)
-    offline_elbo_errors = (offline_elbos - oracle_elbo) / jnp.abs(oracle_elbo)
-    offline_grad_elbo_errors = (jnp.linalg.norm(offline_grads_elbo, axis=1, ord=1) - norm_oracle_grad) / norm_oracle_grad
     # print('Offline ELBOs',offline_elbos)
     # print('Offline grad norm ELBO', offline_grads_elbo)
 
@@ -123,16 +128,20 @@ def experiment(key, exp_id, exp_name):
 
 
     online_elbos_2, online_grads_elbo_2 = jax.jit(jax.vmap(online_elbo_and_grad_func_2, in_axes=(0,None)))(jax.random.split(key, num_replicas), obs_seq)
-    online_elbo_errors_2 = (online_elbos_2 - oracle_elbo) / jnp.abs(oracle_elbo)
-    online_grad_elbo_errors_2 = (jnp.linalg.norm(online_grads_elbo_2, axis=1, ord=1) - norm_oracle_grad) / norm_oracle_grad
+    online_elbo_errors_2 = jax.vmap(scalar_relative_error, in_axes=(None,0))(oracle_elbo, online_elbos_2)
+    online_grad_elbo_errors_2 = jax.vmap(cosine_similarity, in_axes=(None, 0))(oracle_grad_elbo, online_grads_elbo_2)
+    
+    offline_elbos, offline_grads_elbo = jax.jit(jax.vmap(offline_elbo_and_grad_func, in_axes=(0,None)))(jax.random.split(key, num_replicas), obs_seq)
+    offline_elbo_errors = jax.vmap(scalar_relative_error, in_axes=(None,0))(oracle_elbo, offline_elbos)
+    offline_grad_elbo_errors = jax.vmap(cosine_similarity, in_axes=(None, 0))(oracle_grad_elbo, offline_grads_elbo)
 
     online_elbos, online_grads_elbo = jax.jit(jax.vmap(online_elbo_and_grad_func, in_axes=(0,None)))(jax.random.split(key, num_replicas), obs_seq)
-    online_elbo_errors = (online_elbos - oracle_elbo) / jnp.abs(oracle_elbo)
-    online_grad_elbo_errors = (jnp.linalg.norm(online_grads_elbo, axis=1, ord=1) - norm_oracle_grad) / norm_oracle_grad
+    online_elbo_errors = jax.vmap(scalar_relative_error, in_axes=(None,0))(oracle_elbo, online_elbos)
+    online_grad_elbo_errors = jax.vmap(cosine_similarity, in_axes=(None, 0))(oracle_grad_elbo, online_grads_elbo)
 
 
-    elbo_errors = pd.DataFrame(jnp.array([offline_elbo_errors, online_elbo_errors, online_elbo_errors_2]).T, columns=['Offline', 'Online', f'{exp_name}'])
-    grad_elbo_errors = pd.DataFrame(jnp.array([offline_grad_elbo_errors, online_grad_elbo_errors, online_grad_elbo_errors_2]).T, columns=['Offline', 'Online', f'{exp_name}'])
+    elbo_errors = pd.DataFrame(jnp.array([offline_elbo_errors, online_elbo_errors, online_elbo_errors_2]).T, columns=['Offline', 'Online backprop', f'{exp_name}'])
+    grad_elbo_errors = pd.DataFrame(jnp.array([offline_grad_elbo_errors, online_grad_elbo_errors, online_grad_elbo_errors_2]).T, columns=['Offline', 'Online backprop', f'{exp_name}'])
 
     elbo_errors.plot(kind='box')
     plt.savefig(os.path.join(path, f'elbo_errors_{exp_id}'))
@@ -142,7 +151,7 @@ def experiment(key, exp_id, exp_name):
 
 for exp_id in range(3):
     key, subkey = jax.random.split(key, 2)
-    experiment(subkey, exp_id, 'online')
+    experiment(subkey, exp_id, 'Online additive')
 
 # elbo_errors.plot(kind='box')
 # elbo, grad_elbo = oracle_elbo_and_grad_func(obs_seq)