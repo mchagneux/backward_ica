1a11d98282d4ecfb5431b70960d1d3e9a85e8cac
Mathis <mathis.chagneux@telecom-paris.fr>
2023-05-23 14:00:24 +0200

    all models work

--- train_multiple_sequential_models.py
+++ train_multiple_sequential_models.py
@@ -5,8 +5,8 @@ p_model = 'chaotic_rnn'
 base_dir = os.path.join('experiments', f'p_{p_model}')
 
 
-settings_list = ['johnson_backward.10.adam,1e-3,cst.online,50,reset.autodiff_on_backward',
-                 'johnson_backward.10.adam,1e-3,cst.online,50.score,paris,variance_reduction,bptt_depth_2']
+settings_list = ['johnson_backward.10.adam,1e-3,cst.online,10,reset.score,paris,variance_reduction,bptt_depth_2',
+                 'johnson_backward.10.adam,1e-3,cst.online,10,reset.autodiff_on_backward']
 
 
 num_epochs = 1000
